949d98a476e25d890211d5db611d6643a4ec8cfb
ogabek <norpulatovogabek42@gmail.com>
2022-06-19 14:26:43 +0500

    task1

--- bin/arithmetic01.dart
+++ bin/arithmetic01.dart
@@ -2,7 +2,10 @@
 // Create a function argument x of type int.
 // Create a parameter int type y of type int.
 // return sum of two parameters.
+int add(int x, int y) {
+  return x + y;
+}
 
 void main() {
-
+  print(add(10, 90));
 }

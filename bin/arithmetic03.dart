02188ce3afac84468ed16ecdfd8806d5f5cb4329
ogabek <norpulatovogabek42@gmail.com>
2022-06-19 14:45:42 +0500

    task3

--- bin/arithmetic03.dart
+++ bin/arithmetic03.dart
@@ -1,5 +1,10 @@
 // Create a function called func.
 // CCreate function arguments a and b of type int.
 // Returns the remainder of a and b.
+int func(a, b) {
+  return a % b;
+}
 
-void main() {}
+void main() {
+  print(func(46, 3));
+}

64c21754b3de2b11d65e3b3c25f3df9ad029d60f
ogabek <norpulatovogabek42@gmail.com>
2022-06-19 14:39:21 +0500

    task2

--- bin/arithmetic02.dart
+++ bin/arithmetic02.dart
@@ -1,7 +1,10 @@
 // Create a function called divide.
 // Create function arguments a and b of type int.
 // Returns the floor division of a and b.
+int divide(a, b) {
+  return (a ~/ b);
+}
 
 void main() {
-  
+  print(divide(67, 23));
 }
